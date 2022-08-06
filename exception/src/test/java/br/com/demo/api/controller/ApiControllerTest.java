34cd326e89cb910075bf70b9426783e967797ccf
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-06 11:22:05 -0300

    0.10 - Implementação teste unidade com retorno da badRequest.

--- exception/src/test/java/br/com/demo/api/controller/ApiControllerTest.java
+++ exception/src/test/java/br/com/demo/api/controller/ApiControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -23,12 +24,13 @@ class ApiControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("GET /api/demo-exceptions teste do retondo a HandlerException")
-    void testaApiController() throws Exception {
+    @DisplayName("GET /api/parse-int/x teste envia uma string e testa o tratatamento da exceção.")
+    void parseIntValue_entaoBadRequest() throws Exception {
 
         // Executa a request GET
-        mockMvc.perform(get("/api/demo-exceptions"))
+        mockMvc.perform(get("/api/parse-int/x")
                 // Valida o http status code e a content type retornado.
-                .andExpect(status().isOk());
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
