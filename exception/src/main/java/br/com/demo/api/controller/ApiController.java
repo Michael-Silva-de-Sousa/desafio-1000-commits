a9a8d3cbbfac36d8867f3122620f63f053e48265
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-06 11:23:22 -0300

    0.11 - Ajuste da Contoller para lançar exceção customizada. - Inclusão da dependência lang3.

--- exception/src/main/java/br/com/demo/api/controller/ApiController.java
+++ exception/src/main/java/br/com/demo/api/controller/ApiController.java
@@ -1,19 +1,25 @@
 package br.com.demo.api.controller;
 
+import br.com.demo.api.controller.exception.IntegerInvalidException;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/demo-exceptions")
+@RequestMapping("/api/parse-int")
 @RestController
 public class ApiController {
 
-    @GetMapping
-    public ResponseEntity<?> handlerException() {
+    @GetMapping("/{value}")
+    public ResponseEntity<?> handlerException(@PathVariable("value") String value) {
 
-        Integer.parseInt("wyz");
-
+        if(NumberUtils.isParsable(value)) {
+            Integer.parseInt(value);
+        } else {
+            throw new IntegerInvalidException("Não pode parsear o valor " + value + " não é númerico.");
+        }
         return ResponseEntity.ok().build();
     }
 }
