c8334d7ba7eb118bc082f80eb3adaeb374792daf
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-08 20:47:14 -0300

    0.14 - Implementação do Consumer RabbitMQ.

---
+++ consumer/src/main/java/br/com/consumer/QueueConsumer.java
@@ -0,0 +1,16 @@
+package br.com.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class QueueConsumer {
+
+    @RabbitListener(queues = {"${queue.name}"})
+    public void receive(@Payload String fileBody) {
+        log.info("Message {}" + fileBody);
+    }
+}
