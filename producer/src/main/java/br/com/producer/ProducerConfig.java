2bba19618ef9f2e5b28514060d5cb9380f22e42b
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-09 07:55:13 -0300

    0.16 - Configurado o Producer para enviar mensagens para o exchange do RabbitMQ.

--- producer/src/main/java/br/com/producer/ProducerConfig.java
+++ producer/src/main/java/br/com/producer/ProducerConfig.java
@@ -1,5 +1,8 @@
 package br.com.producer;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,5 +18,15 @@ public class ProducerConfig {
     public Queue queue() {
         return new Queue(message, true) {
         };
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange("direct-exchange");
+    }
+
+    @Bean
+    public Binding testeBinding(Queue testeQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(testeQueue).to(exchange).with("key-pegasus");
     }
 }
--- producer/src/main/java/br/com/producer/QueueController.java
+++ producer/src/main/java/br/com/producer/QueueController.java
@@ -1,20 +1,28 @@
 package br.com.producer;
 
 import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/queue")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class QueueController {
 
     private final QueueSender queueSender;
+    private final AmqpTemplate queueSenderExchange;
 
-    @GetMapping
+    @GetMapping("/queue")
     public String send(){
         queueSender.send("test message");
+        return "ok. done";
+    }
+
+    @GetMapping("/exchange")
+    public String sendExchange(){
+        queueSenderExchange.convertAndSend("exchange-pegasus", "key-pegasus", "Meteoro de Pegasus!!!");
         return "ok. done";
     }
 }
