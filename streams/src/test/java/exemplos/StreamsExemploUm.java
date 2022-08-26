3d269fc2f771217c467d3ccca00dfc0e5aad74b2
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-25 21:37:26 -0300

    0.27 - Teste de exemplo filtros faseados.

--- streams/src/test/java/exemplos/StreamsExemploUm.java
+++ streams/src/test/java/exemplos/StreamsExemploUm.java
@@ -18,5 +18,32 @@ class StreamsExemploUm {
         emails.forEach(System.out::println);
     }
 
+    @Test
+    void testeFiltrosEmFasePrecosCarros() throws Exception {
+
+        /**
+         * Exemplo de como usar stream para realizar o
+         * filtro faseado. nesse exemplo primeiro é filtrado os carros
+         * com preço menor de 10.000, em seguida é extraído somente os preços
+         * e depois os preços são multiplicado por .14.
+         * */
+
+        MockData.getCarros()
+                .stream()
+                .filter(car -> { //Filtra todos os carros com preço menor que 10.000
+                    //System.out.println("carros filtrados: " + car);
+                    return car.getPrice() < 10000;
+                })
+                .map(car -> { //Pega somente a propriedade precos
+                    //System.out.println("Carros filtrados: " + car);
+                    return car.getPrice();
+                })
+                .map(price -> { // realiza o calculo dos preços
+                    return price + (price * .14);
+                })
+                .forEach(System.out::println);
+
+
+    }
 
 }
