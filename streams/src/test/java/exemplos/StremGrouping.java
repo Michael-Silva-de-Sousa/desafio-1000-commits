6a395ce1a83335d3a37faba6c2126fdfe5a15d79
Michael Sousa <michael.sp.contato@gmail.com>
2022-08-26 21:37:18 -0300

    0.31 - Implementado exemplo de filtro com agrupamento e count.

--- streams/src/test/java/exemplos/StremGrouping.java
+++ streams/src/test/java/exemplos/StremGrouping.java
@@ -4,8 +4,10 @@ import beans.Pessoa;
 import mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StremGrouping {
@@ -22,5 +24,29 @@ public class StremGrouping {
             pessoas.forEach(System.out::println);
             System.out.println("------" + s);
         });
+    }
+
+    @Test
+    public void agrupandoNomesEcontandoAsOcorrencias() throws Exception {
+
+        List<String> names = List.of(
+                "John",
+                "John",
+                "Mariam",
+                "Alex",
+                "Mohammado",
+                "Mohammado",
+                "Vincent",
+                "Alex",
+                "Alex"
+        );
+
+        Map<String, Long> map = names.stream()
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ));
+
+        System.out.println(map);
     }
 }
